5e4f3297946593c9b214c0ce311e44244833a1b8
dipen1000 <dipenthapa1100@gmail.com>
2024-12-17 20:51:33 +0545

    logout/login

--- project/settings.py
+++ project/settings.py
@@ -123,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'food:index'
+LOGOUT_REDIRECT_URL = 'users:login'
