712ef3116f8f312409e3e3b13a9f9e177133323b
dipen1000 <dipenthapa1100@gmail.com>
2024-12-18 18:40:34 +0545

    'signals'

--- project/settings.py
+++ project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,3 +127,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'food:index'
 LOGOUT_REDIRECT_URL = 'users:login'
+
+LOGIN_URL = 'users:login'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'pictures')
+MEDIA_URL = '/pictures/'
--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from . import views
 from django.contrib.auth import views as authentication_views
 from django.contrib.auth import views as auth_views
+
+from . import views
 
 app_name = 'users'
 
@@ -9,6 +10,8 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', authentication_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path("logout/", auth_views.LogoutView.as_view(http_method_names=["post", "get", "options"], template_name='users/logout.html'), name='logout'),
-
+    path('profile/', views.profilePage, name='profile')
+    
+    
     # path('logout/', authentication_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-]
+] 
